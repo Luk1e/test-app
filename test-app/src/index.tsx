32466a4f3351184d7dc4c1608a90f00aa233e2c5
Luk1e <luka.gogiashvili.02@gmail.com>
2025-12-03 20:58:06 +0400

    init: base setup

---
+++ test-app/src/index.tsx
@@ -0,0 +1,14 @@
+// React & libraries
+import ReactDOM from "react-dom/client";
+
+// Components
+import App from "./App";
+
+// Utils & assets
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<App />);
