b2884ccab636863dd3901af3ff39657ed8500071
Luk1e <luka.gogiashvili.02@gmail.com>
2025-12-03 21:05:10 +0400

    feat: setup router

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,5 +1,20 @@
+// React & libraries
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+// Utils & assets
+import "./App.scss";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/dashboard" element={<div>dashboard</div>} />
+        <Route path="/login" element={<div>login</div>} />
+        <Route path="*" element={<div>error</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
